ebcde7455c096cc4567b41e4a50fb0bcda8bc784
sjones6 <jones.spencera@gmail.com>
2018-11-07 22:28:16 -0500

    use lodash

--- src/view.js
+++ src/view.js
@@ -1,6 +1,8 @@
+import _ from 'lodash'
+
 export default (state, actions) => (
   <div>
-    <h1 id="title" style={`color: ` + state.color }>Yay, Hyperapp and JSX! <span>{state.color}</span></h1>
+    <h1 id="title" style={`color: ` + state.color }>Yay, Hyperapp and JSX! <span>{_.get(state, 'color')}</span></h1>
     <button onclick={state.interval ? actions.stopCyclingColors : actions.startCyclingColors}>{state.interval ? 'stop ' : 'start'} cycling colors</button>
   </div>
 )
