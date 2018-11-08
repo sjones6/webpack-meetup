0a4a6b1e5ee00b537f6f767b07baef62360e16c6
sjones6 <jones.spencera@gmail.com>
2018-11-07 22:30:55 -0500

    add webpack-bundle-analyzer

---
+++ webpack.analyze.js
@@ -0,0 +1,9 @@
+const { BundleAnalyzerPlugin } = require('webpack-bundle-analyzer')
+const prod = require('./webpack.prod.js')
+const merge = require('webpack-merge')
+
+module.exports = merge(prod, {
+  plugins: [
+    new BundleAnalyzerPlugin()
+  ]
+})
